2188b03bf54b2425ef91e0f9f615499bbeac18db
steffFrank <--replace-all>
2022-12-02 18:03:43 +0100

    feat: Styling the quote

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -6,11 +6,11 @@ import { TwitterBtn } from "../TwitterBtn/TwitterBtn";
 export const Quote = () => {
     return (
         <>
-            <p>
+            <p className="quote">
                 <i className="fas fa-quote-left"></i>
                 <span className="quote__text">What you are is what you have been.What you'll be is what you do now.</span>
-                <p className="quote__author">Buddha</p>
             </p>
+            <p className="quote__author">Buddha</p>
             <div className="btn-container">
                 <TwitterBtn />
                 <NewQuote />

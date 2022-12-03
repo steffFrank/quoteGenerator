5976a76d614f5646e41247539481ee6ce95608a1
steffFrank <--replace-all>
2022-12-03 11:01:32 +0100

    feat: Fetch the data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./app.css";
 import { QuoteContainer } from "./components/QuoteContainer/QuoteContainer";
-
+import { LoadingSpinner } from "./components/LoadingSpinner/LoadingSpinner";
 
 const App = () => {
+    
+    const [quotes, setQuotes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const getQuotes = async () => {
+        const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+        try {
+            const response = await fetch(apiUrl);
+            const fetchedData = await response.json();
+            setQuotes(fetchedData);
+            setIsLoading(false);
+        }catch(error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getQuotes();
+    }, [])
 
     return (
-        <QuoteContainer/>
+        isLoading ? <div>loading...</div> : <QuoteContainer quotes={quotes}/>  
     )
 }
 
